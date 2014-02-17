58478381a1bc89d40ae3313c73d9da924cf1c9dc
Joe McIlvain <joe.eli.mac@gmail.com>
2014-02-16 17:47:48 -0800

    Passes not_spec (except one example)

--- sandbox.rb
+++ sandbox.rb
@@ -979,4 +979,17 @@ class << Object.new
     end
   end
   
+  parse "(not true)" do
+    [:call, [:true], :!, [:arglist]]
+  end
+
+  parse <<-ruby do
+      a = 1
+      b = !a
+    ruby
+
+    [:block,
+     [:lasgn, :a, [:lit, 1]],
+     [:lasgn, :b, [:call, [:lvar, :a], :!, [:arglist]]]]
+  end
 end
